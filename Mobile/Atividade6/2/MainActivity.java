ec32376d3ad0b2471831990c52e0128e7ba481a3
Arthur Araujo <arthuraraujo@sempreceub.com>
2026-09-09 10:47:13 -0300

    Add files via upload

---
+++ Mobile/Atividade6/2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.exemplo2intentimplicita;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtTelefone;
+    Button btnEfetuarLigacao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        edtTelefone = (EditText) findViewById(R.id.txtTelefone);
+        btnEfetuarLigacao = (Button) findViewById(R.id.cmdEfetuarLigacao);
+        btnEfetuarLigacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String numeroTelefone = edtTelefone.getText().toString();
+                Intent intencao = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", numeroTelefone, null));
+                startActivity(intencao);
+            }
+        });
+
+    }
+}
